26d68d562a3e10fe10e7169e319f4e4a05453a6a
SHARANKR <sharankr798@gmail.com>
2026-03-24 14:48:42 +0530

    6. Load_params function added for YAML file

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
 import logging
+import yaml
 
 dir = 'log'
 os.makedirs(dir,exist_ok=True)
@@ -21,3 +22,18 @@ formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path:str) -> dict:
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parametes are retreived from %s', params_path)
+        return params
+    except FileNotFoundError as e:
+        logger.error('File not found at %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error %s', e)
+        raise
